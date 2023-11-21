de218d3377524ca2defce9a01e3c295b5fd1be4f
Hes-tia <yanisbenhatat@gmail.com>
2023-11-21 16:44:24 +0100

    21/11/23

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+//console.log("hello world")
+/*
+
+let nomUtilisateur
+let ageUtilisateur
+
+nomUtilisateur = prompt("prenom")
+ageUtilisateur = prompt("age")
+
+console.log(`bonjour ${nomUtilisateur} vous avez ${ageUtilisateur}ans`)
+
+premier script*/
+
+let randomTarget=Math.floor(Math.random()*100+1)
+console.log(randomTarget)
+
+let userNumber=null
+let count= 0
+
+
+
+/*
+while(userNumber!=randomTarget){  
+    count++
+    userNumber=prompt("deviner un nombre en 1 et 100")
+    if(isNaN(userNumber)){alert("deviner un nombre en 1 et 100")}
+    else if(userNumber>randomTarget){alert("c'est moins")}
+    else if(userNumber<randomTarget){alert("c'est plus")}
+    else{alert(`tu as gagné try=${count}`)}
+    
+} */
